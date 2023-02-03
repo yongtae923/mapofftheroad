a7681b4afa82b39e19e24879ae48dcc9f85a9cbb
Yongtae Kim <yongtae7481@gmail.com>
2023-02-03 18:48:13 +0900

    Initial commit

---
+++ src/main/kotlin/com/roadoffthemap/roadoffthemap/RoadoffthemapApplication.kt
@@ -0,0 +1,11 @@
+package com.roadoffthemap.roadoffthemap
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoadoffthemapApplication
+
+fun main(args: Array<String>) {
+	runApplication<RoadoffthemapApplication>(*args)
+}
---
+++ src/test/kotlin/com/roadoffthemap/roadoffthemap/RoadoffthemapApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.roadoffthemap.roadoffthemap
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RoadoffthemapApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
